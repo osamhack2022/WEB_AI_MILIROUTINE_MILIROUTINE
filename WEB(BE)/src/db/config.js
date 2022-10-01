8010f688675a4abc6dfccc2f9802a4d45904192e
김민찬 <cmk0487@naver.com>
2022-10-01 02:48:28 +0000

    Fix : DB port 문제 해결 (github codespace에선 작동 X)

--- WEB(BE)/src/db/config.js
+++ WEB(BE)/src/db/config.js
@@ -7,7 +7,8 @@ const db = mysql.createConnection({
 	host : process.env.DB_HOST,
 	user : process.env.DB_USER,
 	password : process.env.DB_PASSWORD,
-	database : process.env.DB_DATABASE
+	database : process.env.DB_DATABASE,
+	port : '3000'
 })
 
 db.connect();
