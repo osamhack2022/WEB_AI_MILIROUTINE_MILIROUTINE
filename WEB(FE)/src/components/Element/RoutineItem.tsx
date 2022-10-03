b47deeccd8b039e59f45d41d731278c9ab190ff8
권재원 <springkjw@gmail.com>
2022-10-03 22:34:06 +0900

    MiliRoutine Item Component

---
+++ WEB(FE)/src/components/Element/RoutineItem.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/Element";
+
+export interface RoutineItemProps {
+  size?: number;
+  nickname?: string;
+  title?: string;
+}
+
+export const RoutineItem = ({
+  size = 40,
+  nickname = "Test Nickname",
+  title = "Test Title",
+}: RoutineItemProps) => {
+  const itemSize = `w-${size} h-${size}`;
+
+  return (
+    <div className={itemSize}>
+      <div className={`border rounded-xl border-black mb-2 ${itemSize}`} />
+      <span className="text-sm text-gray-500">{nickname}</span>
+      <h4 className="text-lg text-black font-bold mb-1">{title}</h4>
+      <Button label="학습" />
+      <div className="flex flex-row justify-between items-center mt-4">
+        <span className="text-black text-sm">주 5회 인증</span>
+        <span className="text-black text-sm">35명 참여중</span>
+      </div>
+    </div>
+  );
+};
--- WEB(FE)/src/components/Element/index.ts
+++ WEB(FE)/src/components/Element/index.ts
@@ -2,3 +2,4 @@ export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { Button } from "./Button";
 export { Jumbotron } from "./Jumbotron";
+export { RoutineItem } from "./RoutineItem";
