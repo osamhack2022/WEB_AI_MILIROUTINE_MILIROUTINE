f3504fe4d755b20403ff2b3ac50c48aeb9f3efcb
dong97338 <32699584+dong97338@users.noreply.github.com>
2022-10-07 09:17:46 +0000

    feat: #1 레이어 구조 수정
    입력층: 루틴 이름 벡터(300)+주기 벡터(30)
    출력층: 루틴 30가지의 원핫벡터

--- AI/Net_model.py
+++ AI/Net_model.py
@@ -6,13 +6,13 @@ class Net_model(nn.Module):
     def __init__(self): # *hint* dropout probability를 위한 파라미터 추가
         super().__init__()
         self.mlp = nn.Sequential(
-            nn.Linear(400, 300), # 첫 번째 층 정의
+            nn.Linear(330, 220), # 첫 번째 층 정의: 등록한 루틴의 이름 평균 벡터 300 + 주기 벡터 30
             nn.init.xavier_normal_(torch.empty(1,1)), #등을 통해 weight initialization 적용
             nn.ReLU(), # activation function
             # *hint* nn.Dropout(drop_prob) 등을 통해 dropout 적용
-            nn.Linear(300, 200),
+            nn.Linear(220, 110),
             nn.ReLU(),
-            nn.Linear(200, 1),
+            nn.Linear(110, 30), # 출력 층 정의: 벡터 등록한 루틴 + 새로 등록할 루틴의 1 or 0 원핫벡터
             nn.Softmax(dim=-1))
 
     def forward(self, x):
