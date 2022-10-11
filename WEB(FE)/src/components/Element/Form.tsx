f98fdaa7cad09cb056e79baa40ef69d9b2723c69
권재원 <springkjw@gmail.com>
2022-10-11 20:09:04 +0900

    회원가입 폼

--- WEB(FE)/src/components/Element/Form.tsx
+++ WEB(FE)/src/components/Element/Form.tsx
@@ -1,13 +1,24 @@
+import { ChangeEvent } from "react";
+
 export interface FormProps {
   label?: string;
   help?: string;
   placeholder?: string;
   type: string;
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Form = ({ label, help, type, placeholder }: FormProps) => {
+export const Form = ({
+  label,
+  help,
+  type,
+  placeholder,
+  value,
+  onChange,
+}: FormProps) => {
   return (
-    <div className="mb-10">
+    <div className="mb-8">
       <div className="my-2 flex flex-row items-center">
         {!!label ? <p className="text-black text-sm mr-6">{label}</p> : null}
         {!!help ? <p className="text-gray-400 text-sm">{help}</p> : null}
@@ -18,6 +29,8 @@ export const Form = ({ label, help, type, placeholder }: FormProps) => {
           className="w-full border border-black h-14 py-2 px-4"
           type={type}
           placeholder={placeholder}
+          value={value}
+          onChange={onChange}
         />
       </div>
     </div>
