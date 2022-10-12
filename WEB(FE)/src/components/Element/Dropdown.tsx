a3f6e488ee8467fa3a91ef7afd8efad020448aa0
권재원 <springkjw@gmail.com>
2022-10-12 23:09:46 +0900

    Dropdown 컴포넌트

--- WEB(FE)/src/components/Element/Dropdown.tsx
+++ WEB(FE)/src/components/Element/Dropdown.tsx
@@ -1,3 +1,7 @@
+import { Fragment, useState, useEffect, useCallback } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
+
 export interface DropdownItem {
   label: string;
   value: string;
@@ -5,16 +9,97 @@ export interface DropdownItem {
 
 export interface DropdownProps {
   items?: Array<DropdownItem>;
+  value?: DropdownItem;
+  onChange?: (item: DropdownItem) => void;
 }
 
-export const Dropdown = ({ items = [] }: DropdownProps) => {
+function classNames(...classes: Array<string>) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export const Dropdown = ({ items = [], value }: DropdownProps) => {
+  const [selected, setSelected] = useState<DropdownItem | undefined>();
+
+  useEffect(() => {
+    if (value) {
+      setSelected(value);
+    }
+  }, [value]);
+
+  const onSelect = useCallback((v: DropdownItem) => {
+    setSelected(v);
+  }, []);
+
   return (
-    <div className="relative w-full lg:max-w-sm">
-      <select className="w-full p-2.5 text-black bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-orange">
-        {items.map((item) => {
-          return <option>{item.label}</option>;
-        })}
-      </select>
-    </div>
+    <>
+      <Listbox as="div" value={selected} onChange={onSelect}>
+        {({ open }) => (
+          <div className="relative mt-1">
+            <Listbox.Button className="relative w-full cursor-default border border-black bg-white h-14 py-2 px-2 text-left shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
+              <span className="flex items-center">
+                {selected ? (
+                  <span className="ml-3 block truncate">{selected.label}</span>
+                ) : null}
+              </span>
+              <span className="pointer-events-none absolute inset-y-0 right-2 ml-3 flex items-center pr-2">
+                <ChevronDownIcon
+                  className="h-10 w-10 text-black"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto bg-white border border-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {items.map((item, itemIdx) => (
+                  <Listbox.Option
+                    key={itemIdx}
+                    className={({ active }) =>
+                      classNames(
+                        active ? "text-white bg-indigo-600" : "text-black",
+                        "relative cursor-default select-none h-14 py-4 pl-3 pr-9"
+                      )
+                    }
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <span
+                            className={classNames(
+                              selected ? "font-semibold" : "font-normal",
+                              "ml-3 block truncate"
+                            )}
+                          >
+                            {item.label}
+                          </span>
+                        </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? "text-white" : "text-indigo-600",
+                              "absolute inset-y-0 right-0 flex items-center pr-4"
+                            )}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        )}
+      </Listbox>
+    </>
   );
 };
--- WEB(FE)/src/features/routine/RoutineMake.tsx
+++ WEB(FE)/src/features/routine/RoutineMake.tsx
@@ -1,8 +1,24 @@
 import { useState, useCallback, ChangeEvent } from "react";
-import { Form } from "@/components/Element";
+import { Form, Select, DropdownItem } from "@/components/Element";
 
 export const RoutineMakePage = () => {
-  const [title, setTitle] = useState("");
+  const [categories, setCategories] = useState([
+    { label: "학습1", value: "0" },
+    { label: "학습2", value: "1" },
+  ]);
+  const [intervals, setIntervals] = useState([
+    { label: "주기1", value: "0" },
+    { label: "주기2", value: "1" },
+  ]);
+  const [periods, setPeriods] = useState([
+    { label: "기간1", value: "0" },
+    { label: "기간2", value: "1" },
+  ]);
+
+  const [title, setTitle] = useState<string | undefined>("");
+  const [category, setCategory] = useState<DropdownItem | undefined>();
+  const [interval, setInterval] = useState<DropdownItem | undefined>();
+  const [period, setPeriod] = useState<DropdownItem | undefined>();
 
   const onChangeTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     e.persist();
@@ -26,6 +42,13 @@ export const RoutineMakePage = () => {
             value={title}
             onChange={onChangeTitle}
           />
+          <Select
+            label="밀리루틴 카테고리"
+            items={categories}
+            value={category}
+          />
+          <Select label="인증 주기" items={intervals} value={interval} />
+          <Select label="루틴 진행 기간" items={periods} value={period} />
         </div>
       </div>
     </>
