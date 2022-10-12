1b6042145a239216348c4ac1f702fdda4bcc3fb2
dong97338 <32699584+dong97338@users.noreply.github.com>
2022-10-12 10:31:45 +0000

    fix: #6 consql.py 계정 정보 제거
    #32 따로 a6t.py 로컬에 만들어서 저장/import

--- AI/consql.py
+++ AI/consql.py
@@ -1,40 +1,18 @@
 import pymysql
+import a5t  # sql 아이디 비밀번호(코드스페이스에만, 커밋x)
 
 host="52.79.92.84"
-user="miliroutine_developer"
-password="2022MySQL!@"
+user=a5t.user
+password=a5t.password
 port=53609
 database="miliroutine_db"
 use_unicode=True
 charset='utf8'
 
-# class con(pymysql.Connection):
-#     def __init__(self):
-#         self.host="52.79.92.84"
-#         self.user="miliroutine_developer"
-#         self.password="2022MySQL!@"
-#         self.port=53609
-#         self.database="miliroutine_db"
-#         self.use_unicode=True
-#         self.charset='utf8'
-#     def ex(self,q):
-#         cur=self.connect().cursor()
-#         cur.execute(q)
-#         rows=cur.fetchall()
-#         return rows
-
-# pymysql.Connection.connect()
-
 def ex(q):
-    con=pymysql.connect(host=host, user=user, passwd=password, db=database, port=port, use_unicode=True, charset='utf8')
+    con=pymysql.connect(host=host, user=a5t.user, passwd=a5t.password, db=database, port=port, use_unicode=True, charset='utf8')
     cur=con.cursor()
     cur.execute(q)
     ret=cur.fetchall()
     con.close()
     return ret
-# con=pymysql.connect(host=host, user=user, passwd=password, db=database, port=port, use_unicode=True, charset='utf8')
-# cur = con.cursor()
-# cur.execute('SELECT * FROM user_routine WHERE user_no=1')
-# rows = cur.fetchall()
-# print(rows)
-# con.close()
