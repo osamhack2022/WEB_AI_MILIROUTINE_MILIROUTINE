069d7b7e0a413fd4d700ecc8ec8f04ec86d91628
Yongjun Park <yopark@student.42seoul.kr>
2022-10-14 13:27:07 +0900

    Style: prettier

--- WEB(BE)/app.js
+++ WEB(BE)/app.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const app = express()
+const app = express();
 const bodyParser = require('body-parser');
-const router = require('./src/routes/index')
+const router = require('./src/routes/index');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const PORT = 3000;
 
-app.use(cors({
-	origin : true,
-	credentials : true
-}))
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("views", "./src/views");
-app.set('view engine', 'ejs'); 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.use(router)
+app.use(router);
 
-
-
-app.listen(PORT, function(){
-	console.log("Connected on port " + PORT);
+app.listen(PORT, function () {
+    console.log('Connected on port ' + PORT);
 });
